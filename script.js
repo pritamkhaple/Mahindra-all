75219df428859fbf1a2f670e307fa1f2036a67df
Pritam Khaple <110626364+pritamkhaple@users.noreply.github.com>
2024-01-11 13:58:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
+// For Hamburger mob menu function
+
 let burger = document.getElementById("burg");
 
 burger.addEventListener("click", () => {
     console.log("burger clicked") //just to check
+    document.querySelector("body").style.overflow = "hidden";
         document.querySelector(".mob-nav1-ul").style.display = "block";
         document.querySelector(".nav-div").style.height = "100vh";
         document.querySelector("#burg").style.display = "none";
         document.querySelector(".close").style.display = "block";
         document.querySelector(".banner-txt").style.position = "relative";
+        
 })
 
 let clos = document.getElementById("closed");
 
 clos.addEventListener("click", ()=>{
+    document.querySelector("body").style.overflow = "auto";
     document.querySelector(".mob-nav1-ul").style.display = "none";
     document.querySelector(".nav-div").style.height = "auto";
     document.querySelector("#burg").style.display = "block";
     document.querySelector(".close").style.display = "none";
     document.querySelector(".banner-txt").style.position = "absolute";
+    
     console.log("check close")
 })
+
+// For Top arrow function
+
+document.addEventListener("DOMContentLoaded", function() {
+    let topArrow = document.getElementById("topArrow");
+
+    topArrow.addEventListener("click", () => {
+        // Scroll to the top of the page
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth" // You can use "auto" for instant scrolling
+        });
+    });
+});
